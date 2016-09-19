111b6890f82e45d34d81f1367bbc67af5ce5d9db
Dan Krog <dankroga@gmail.com>
2016-09-19 19:45:27 +0200

    Added Virial theorem

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ Solving orbital problems numerically.
 ## nbody problem.
 Simulation of Earth, Sun, Mars, Mercury and Venus. 
 Based on real parameters(mass, orbit speed) from Wikipedia.
+###Simulation
 ![Orbit.png](https://github.com/Bootlegg/Orbit/blob/master/Orbit.png)
+###Virial Theorem
+Testing the virial theorem
+![Virial.png](https://github.com/Bootlegg/Orbit/blob/master/Virial.png)
--- nbody.py
+++ nbody.py
@@ -42,35 +42,57 @@ nt = 1000
 x = np.linspace(-2*AU,2*AU)
 y = np.linspace(-2*AU,2*AU)
 
-xp = np.zeros((1+nt,Np))
-yp = np.zeros((1+nt,Np))
-axp = np.zeros((1+nt,Np))
-ayp = np.zeros((1+nt,Np))
-up = np.zeros((1+nt,Np))
-vp = np.zeros((1+nt,Np))
+x = np.zeros((1+nt,Np))
+y = np.zeros((1+nt,Np))
+ax = np.zeros((1+nt,Np))
+ay = np.zeros((1+nt,Np))
+u = np.zeros((1+nt,Np))
+v = np.zeros((1+nt,Np))
+T = np.zeros(1+nt)
+V = np.zeros(1+nt)
+
+Tavg = np.zeros(1+nt)
+Vavg = np.zeros(1+nt)
 
 #Earth
-xp[0,3] = AU
-vp[0,3] = 29800
+x[0,3] = AU
+v[0,3] = 29800
 
 #Venus
-xp[0,2] = -108200000*10**3
-vp[0,2] = -35000
+x[0,2] = -108200000*10**3
+v[0,2] = -35000
 
 #mercury
-yp[0,1] = -46001200*10**3
-up[0,1] = 47.362*10**3
+y[0,1] = -46001200*10**3
+u[0,1] = 47.362*10**3
 
 #mars
-yp[0,4] = 1.3814*AU
-up[0,4] = -24.077*10**3
+y[0,4] = 1.3814*AU
+u[0,4] = -24.077*10**3
 
-#jupiter
-yp[0,5] = 778500000*10**3
-up[0,5] = -13.07*10**3
+#juiter
+y[0,5] = 778500000*10**3
+u[0,5] = -13.07*10**3
 
+#Initial kinetic and potential energy
+T[0] = np.array([0.5*m[i]*(u[0,i]**2+v[0,i]**2) for i in range(Np)]).sum()
 
-for n in range(nt):
+for i in range(Np):
+	for j in range(i+1,Np):
+		V[0] = V[0] + (-G*m[i]*m[j]/np.sqrt((x[0,i]-x[0,j])**2+(y[0,i]-y[0,j])**2))
+
+#Basic stormer-verlet integration, first timestep
+#Accelerations[0]
+for i in range(Np):
+		for j in range(Np):
+			if j != i:
+				ax[0,i] = ax[0,i]+(-G*m[j]/((x[0,i]-x[0,j])**2+(y[0,i]-y[0,j])**2)**(1.5))*(x[0,i]-x[0,j])
+				ay[0,i] = ay[0,i]+(-G*m[j]/((x[0,i]-x[0,j])**2+(y[0,i]-y[0,j])**2)**(1.5))*(y[0,i]-y[0,j])
+#positions step 1
+x[1,:] = x[0,:] + u[0,:]*dt+0.5*ax[0,:]*dt**2 
+y[1,:] = y[0,:] + v[0,:]*dt+0.5*ay[0,:]*dt**2 
+
+for n in range(1,nt):
 	#the equations
 	#dxe/dt = ue
 	#due/dt = Fx/m
@@ -79,41 +101,60 @@ for n in range(nt):
 	#ue[1] = ue[0]+dt*Fx/m
 	#ve[1] = ve[0]+dt*Fy/m
 	
-	#Make acceleration
+	#Acceleration
 	for i in range(Np):
 		for j in range(Np):
-			if j != i:
-				axp[n+1,i] = axp[n+1,i]+(-G*m[j]/((xp[n,i]-xp[n,j])**2+(yp[n,i]-yp[n,j])**2)**(1.5))*(xp[n,i]-xp[n,j])
-				ayp[n+1,i] = ayp[n+1,i]+(-G*m[j]/((xp[n,i]-xp[n,j])**2+(yp[n,i]-yp[n,j])**2)**(1.5))*(yp[n,i]-yp[n,j])
+			if j != i: #Here, either do n or n+1
+				ax[n,i] = ax[n,i]+(-G*m[j]/((x[n,i]-x[n,j])**2+(y[n,i]-y[n,j])**2)**(1.5))*(x[n,i]-x[n,j])
+				ay[n,i] = ay[n,i]+(-G*m[j]/((x[n,i]-x[n,j])**2+(y[n,i]-y[n,j])**2)**(1.5))*(y[n,i]-y[n,j])
 				
 				
-	#for i in range(Np):
-	#	up[n+1,i] = up[n,i] + dt*axp[n+1,i]
-	#	vp[n+1,i] = vp[n,i] + dt*ayp[n+1,i]
-	up[n+1,:] = up[n,:] + dt*axp[n+1,:]
-	vp[n+1,:] = vp[n,:] + dt*ayp[n+1,:]	
-	
-	xp[n+1,:] = xp[n,:]+dt*up[n+1,:]
-	yp[n+1,:] = yp[n,:]+dt*vp[n+1,:]
-	
-	
-	#for i in range(Np):
-		#Euler Forward Method
-		#xp[n+1,i] = xp[n,i]+dt*up[n+1,i]
-		#yp[n+1,i] = yp[n,i]+dt*vp[n+1,i]
+	#update velocity
+	#velocity either n or n+1, n+1 works with Euler Forward
+	u[n,:] = u[n-1,:] + dt*ax[n,:]
+	v[n,:] = v[n-1,:] + dt*ay[n,:]	
+	#update positions
+	#x[n+1,:] = x[n,:]+dt*u[n+1,:]
+	#y[n+1,:] = y[n,:]+dt*v[n+1,:]
 
-		
-		#Verlet integration??
-		#xp[n+1,i] = 2*xp[n,i]-xp[n-1,i]+dt**2*axp[n,i]
-		#yp[n+1,i] = 2*yp[n,i]-yp[n-1,i]+dt**2*ayp[n,i]
-		
+	#update positions verlet
+	x[n+1,:] = 2*x[n,:]-x[n-1,:]+ax[n,:]*dt**2
+	y[n+1,:] = 2*y[n,:]-y[n-1,:]+ay[n,:]*dt**2
+	
+	#Kinetic energy either do n+1 or n...
+	T[n] = np.array([0.5*m[i]*(u[n,i]**2+v[n,i]**2) for i in range(Np)]).sum()
+	#Potential energy
+	for i in range(Np):
+		for j in range(i+1,Np):
+			V[n] = V[n] + (-G*m[i]*m[j]/np.sqrt((x[n,i]-x[n,j])**2\
+							+ (y[n,i]-y[n,j])**2))
+
+	#time average kinetic energy
+	Tavg[n] = np.array(T[:n]).sum()*dt/((n)*dt)
+
+	#time average potential energy
+	Vavg[n] = np.array(V[:n]).sum()*dt/((n)*dt)
+
+
+	##Different schemes
 		#Average of timesteps
-		#xp[n+1,i] = xp[n,i]+dt*(0.5*up[n+1,i]+0.5*up[n,i])
-		#yp[n+1,i] = yp[n,i]+dt*(0.5*vp[n+1,i]+0.5*vp[n,i])
-	
-	
+		#x[n+1,i] = x[n,i]+dt*(0.5*u[n+1,i]+0.5*u[n,i])
+		#y[n+1,i] = y[n,i]+dt*(0.5*v[n+1,i]+0.5*v[n,i])
+
+fig1 = plt.figure(1)
+ax1 = fig1.gca()
+#ax1.plot(T,c='red')
+#ax1.plot(V,c='blue')
+ax1.plot(Tavg[1:-2],c='red')
+ax1.plot(-0.5*Vavg[1:-2],c='blue')
+ax1.set_title('Virial Theorem')
+ax1.set_xlabel('timestep n')
+ax1.set_ylabel('Energy[J]')
+ax1.legend(['T', '-1/2 V'])
+plt.savefig('Virial.png', bbox_inches='tight')
+
 plt.hold(True)
-fig = plt.figure()
+fig = plt.figure(2)
 ax = fig.gca()
 ax.set_xlim(-2*AU,2*AU)
 ax.set_ylim(-2*AU,2*AU)
@@ -132,8 +173,8 @@ def animate(i): #i increment with 1 each step
 	
 	colors = ['yellow','black','black','blue','red','black']
 	for j in range(Np):
-		ax.plot(xp[0:i,j], yp[0:i,j],c=colors[j])
-		ax.scatter(xp[i,j], yp[i,j],s=30,c=colors[j])
+		ax.plot(x[0:i,j], y[0:i,j],c=colors[j])
+		ax.scatter(x[i,j], y[i,j],s=30,c=colors[j])
 	
 	if i == 50:
 		plt.savefig('Orbit.png', bbox_inches='tight')
