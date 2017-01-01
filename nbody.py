43b541c3a9ad889682563c498f1f877c2d040c2b
Dan <dankroga@gmail.com>
2017-01-01 15:43:42 +0100

    more class

--- nbody.py
+++ nbody.py
@@ -10,19 +10,75 @@ class Planet:
 	'Planets and objects including Sun'
 	PlanetCount = 0
 	planets = []
-	m = []
 
-	def __init__(self,mass,x0,y0,u0,v0,radius):
-		#x0,y0,u0,v0 are initial conditions for motion
+
+	def __init__(self,
+				mass, 	#mass of planet
+				x0, 	# initial x starting position
+				y0,		# initial y starting position
+				u0,		# initial u velocity
+				v0,		# initial v velocity
+				radius	#radius of planet, not used thus far
+				):
+	
 		self.mass = mass
 		self.radius = radius
 		self.x0 = x0
 		self.y0 = y0
 		self.u0 = u0
 		self.v0 = v0
+
 		Planet.PlanetCount += 1
 		Planet.planets.append(self)
-		Planet.m.append(self.mass)
+
+
+
+	def posvos(nt):
+		#Appending to arrays i numpy er ikke godt, og kræver re-allocation af hele array og 
+		#temporarily doubles memory requirement... så bedst at have size of array til at starte med
+		
+		#Position
+		Planet.xp = np.zeros((nt+1,Planet.PlanetCount))
+		Planet.yp = np.zeros((nt+1,Planet.PlanetCount))
+		
+		#Velocity
+		Planet.up = np.zeros((nt+1,Planet.PlanetCount))
+		Planet.vp = np.zeros((nt+1,Planet.PlanetCount))
+
+		#Acceleration
+		Planet.axp = np.zeros((nt+1,Planet.PlanetCount))
+		Planet.ayp = np.zeros((nt+1,Planet.PlanetCount))
+
+		#Energies
+		Planet.Tkin = np.zeros((1+nt,Planet.PlanetCount))
+		Planet.Vpot = np.zeros((1+nt,Planet.PlanetCount))
+		Planet.Tavg = np.zeros((1+nt,Planet.PlanetCount))
+		Planet.Vavg = np.zeros((1+nt,Planet.PlanetCount))
+
+
+		Planet.m = np.zeros(Planet.PlanetCount)
+
+	def setposmass():
+		for i in range(Planet.PlanetCount):
+			Planet.xp[0,i] = Planet.planets[i].x0
+			Planet.yp[0,i] = Planet.planets[i].y0
+			Planet.up[0,i] = Planet.planets[i].u0
+			Planet.vp[0,i] = Planet.planets[i].v0
+			Planet.m[i] = Planet.planets[i].mass
+
+def CallFortran():
+	(Planet.axp,Planet.ayp,
+	Planet.up,Planet.vp,
+	Planet.xp,Planet.yp,
+	Planet.Tavg,Planet.Vavg,
+	Planet.Tkin,Planet.Vpot) = np.array(mainr.mainr(
+									Planet.axp,Planet.ayp,
+									Planet.up,Planet.vp,
+									Planet.xp,Planet.yp,
+									Planet.m,
+									Planet.Tavg,Planet.Vavg,
+									Planet.Tkin,Planet.Vpot,
+									dt,G,nt,Np))
 
 
 #instance Planet(mass,x,y,u,vradius)
@@ -45,8 +101,6 @@ print("Planet masses")
 for i in range(Planet.PlanetCount):
 	print(Planet.planets[i].mass)
 
-m = np.array(Planet.m)
-
 
 
 #units
@@ -59,40 +113,25 @@ dt = 300000 #300000 #seems about right
 #iterations
 nt = 1000
 
-x = np.zeros((1+nt,Np))
-y = np.zeros((1+nt,Np))
-ax = np.zeros((1+nt,Np))
-ay = np.zeros((1+nt,Np))
-u = np.zeros((1+nt,Np))
-v = np.zeros((1+nt,Np))
 
-Tkin = np.zeros((1+nt,Np))
-Vpot = np.zeros((1+nt,Np))
-Tavg = np.zeros((1+nt,Np))
-Vavg = np.zeros((1+nt,Np))
+Planet.posvos(nt)
+Planet.setposmass()
+CallFortran()
 
+def TVplot():
+	fig1 = plt.figure(1)
+	ax1 = fig1.gca()
+	#ax1.plot(T,c='red')
+	#ax1.plot(V,c='blue')
+	ax1.plot(Planet.Tavg[1:-2,0],c='red')
+	ax1.plot(-0.5*Planet.Vavg[1:-2,0],c='blue')
+	ax1.set_title('Virial Theorem')
+	ax1.set_xlabel('timestep n')
+	ax1.set_ylabel('Energy[J]')
+	ax1.legend(['T', '-1/2 V'])
+	fig1.savefig('figs/Virial.png', bbox_inches='tight')
 
-for i in range(Np):
-	x[0,i] = Planet.planets[i].x0
-	y[0,i] = Planet.planets[i].y0
-	u[0,i] = Planet.planets[i].u0
-	v[0,i] = Planet.planets[i].v0
-
-
-#Call Fortran program
-ax,ay,u,v,x,y,Tavg,Vavg,Tkin,Vpot = np.array(mainr.mainr(ax,ay,u,v,x,y,m,Tavg,Vavg,Tkin,Vpot,dt,G,nt,Np))
-
-fig1 = plt.figure(1)
-ax1 = fig1.gca()
-#ax1.plot(T,c='red')
-#ax1.plot(V,c='blue')
-ax1.plot(Tavg[1:-2,0],c='red')
-ax1.plot(-0.5*Vavg[1:-2,0],c='blue')
-ax1.set_title('Virial Theorem')
-ax1.set_xlabel('timestep n')
-ax1.set_ylabel('Energy[J]')
-ax1.legend(['T', '-1/2 V'])
-fig1.savefig('figs/Virial.png', bbox_inches='tight')
+TVplot()
 
 plt.hold(True)
 fig = plt.figure(2)
@@ -114,8 +153,8 @@ def animate(i): #i increment with 1 each step
 	
 	colors = ['yellow','black','black','blue','red','black']
 	for j in range(Np):
-		ax.plot(x[0:i,j], y[0:i,j],c=colors[j])
-		ax.scatter(x[i,j], y[i,j],s=30,c=colors[j])
+		ax.plot(Planet.xp[0:i,j], Planet.yp[0:i,j],c=colors[j])
+		ax.scatter(Planet.xp[i,j], Planet.yp[i,j],s=30,c=colors[j])
 	
 	if i == 50:
 		fig.savefig('figs/Orbit.png', bbox_inches='tight')
